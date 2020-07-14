a65c0194b49474139844c6cd64d93df4d22fef5e
JJ299 <62095762+JJ299@users.noreply.github.com>
2020-07-14 19:33:32 -0400

    Added JS

---
+++ script.js
@@ -0,0 +1,9 @@
+// 1. Declare variables nad capture input.
+
+//2. Combine the input with other wrods to create a story
+
+//3. Display the story as a <p> inside the <main> element
+
+const adj = prompt("Please enter an adjective.");
+const verb = prompt("Please enter a verb.");
+const noun = prompt("Please enter a noun.");
