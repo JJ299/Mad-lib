c8c1abb8c05fe6173c1b188e3c08808a8f8ec979
JJ299 <62095762+JJ299@users.noreply.github.com>
2020-07-14 22:58:23 -0400

    Finalized javascript

--- script.js
+++ script.js
@@ -7,3 +7,8 @@
 const adj = prompt("Please enter an adjective.");
 const verb = prompt("Please enter a verb.");
 const noun = prompt("Please enter a noun.");
+
+const final = `<p>There once was a ${adj} programmer who wanted to use Javascript to ${verb} the ${noun}</p>`;
+
+document.querySelector('main').innerHTML = final;
+
